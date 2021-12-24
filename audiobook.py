69c4fb51d628471a44b2120807eaaa098b39e8e3
mcmusonda1 <mcmusondaleonard@gmail.com>
2021-12-24 17:35:16 +0300

    added a book input function

--- audiobook.py
+++ audiobook.py
@@ -1,13 +1,16 @@
 import pyttsx3
 import PyPDF2
 
-book = open('book.pdf', 'rb')
+book = open(input('Enter the name of the book you want to be read: \n'), 'rb')
 Reader = PyPDF2.PdfFileReader(book)
 
-
+print("Am reading from the book you chose, \n relax and enjoy")
 Read = pyttsx3.init()
 for num in range(10, 12):
     page = Reader.getPage(num)
     text = page.extractText()
     Read.say(text)
     Read.runAndWait()
+
+
+print("We are reading from " + book + " " + num)
