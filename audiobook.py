016f0c7558698a26ad42b47660dcebfa916eeedd
mcmusonda1 <mcmusondaleonard@gmail.com>
2021-12-24 17:27:55 +0300

    Audio  book reader addition of a for loop

--- audiobook.py
+++ audiobook.py
@@ -1,5 +1,13 @@
 import pyttsx3
+import PyPDF2
 
-test = pyttsx3.init()
-test.say('hello my love')
-test.runAndWait()
+book = open('book.pdf', 'rb')
+Reader = PyPDF2.PdfFileReader(book)
+
+
+Read = pyttsx3.init()
+for num in range(10, 12):
+    page = Reader.getPage(num)
+    text = page.extractText()
+    Read.say(text)
+    Read.runAndWait()
